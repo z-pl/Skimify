8e9bc4642a569e33075e0ca5679aa461b1da683e
ahad abdul <ahad.a2703@gmail.com>
2023-03-04 22:18:16 +1100

    Added landing content

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import { MainBody } from './Components/MainBody';
 import { Navbar } from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App flex flex-col gap-20">
       <Navbar></Navbar>
+      <MainBody></MainBody>
     </div>
   );
 }
---
+++ my-app/src/Components/MainBody.tsx
@@ -0,0 +1,9 @@
+import { LandingHeader } from "./LandingHeader";
+
+export function MainBody() {
+  return (
+    <div >
+      <LandingHeader></LandingHeader>
+    </div>
+  )
+}
