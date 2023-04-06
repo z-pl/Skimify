3e3cfa2167296cc1ae12ca27a83d0226dce68aef
ahad abdul <ahad.a2703@gmail.com>
2023-04-06 19:23:51 +1000

    add spring boot

---
+++ backend-skimify/src/test/java/com/example/backendskimify/BackendSkimifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendskimify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSkimifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
