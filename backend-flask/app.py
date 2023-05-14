c13210fdd4a7f480d2c230258ce1187808200201
ahad abdul <ahad.a2703@gmail.com>
2023-05-14 20:31:26 +1000

    added python config

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -10,11 +10,11 @@ from cache import cache
 from flask_cors import CORS
 import os
 from dotenv import load_dotenv
-POSTGRES_USER = os.environ['POSTGRES_USER']
-POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD']
-POSTGRES_DB = os.environ['POSTGRES_DB']
-POSTGRES_HOST = os.environ['POSTGRES_HOST']
-POSTGRES_PORT = os.environ['POSTGRES_PORT']
+POSTGRES_USER = os.getenv('POSTGRES_USER') or ""
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD') or ""
+POSTGRES_DB = os.getenv('POSTGRES_DB') or ""
+POSTGRES_HOST = os.getenv('POSTGRES_HOST') or ""
+POSTGRES_PORT = os.getenv('POSTGRES_PORT')  or ""
 
 def create_app(db_url=None):
 
--- backend-flask/cache.py
+++ backend-flask/cache.py
@@ -1,7 +1,7 @@
 import redis
 import os
-REDIS_HOST = os.environ['REDIS_HOST']
-REDIS_PORT = os.environ['REDIS_PORT']
+REDIS_HOST = os.getenv('REDIS_HOST') or 'localhost'
+REDIS_PORT = os.getenv('REDIS_PORT') or 6379
 cache = redis.Redis(
         host=REDIS_HOST, port=REDIS_PORT, db=0,decode_responses=True
 )
