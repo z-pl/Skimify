28f18a6a0db39b132d8929a76d9f8d9d513f20de
ahad abdul <ahad.a2703@gmail.com>
2023-05-17 15:12:40 +1000

    configure redis db

--- backend-flask/cache.py
+++ backend-flask/cache.py
@@ -1,7 +1,13 @@
 import redis
 import os
-REDIS_HOST = os.getenv('REDIS_HOST') or 'localhost'
-REDIS_PORT = os.getenv('REDIS_PORT') or 6379
+REDIS_HOST = os.getenv('REDIS_HOST')
+REDIS_PORT = os.getenv('REDIS_PORT')
+REDIS_USERNAME = os.getenv('REDIS_USERNAME')
+REDIS_PASSWORD = os.getenv('REDIS_PASSWORD')
+
 cache = redis.Redis(
-        host=REDIS_HOST, port=REDIS_PORT, db=0,decode_responses=True
+    host=REDIS_HOST,
+    port=REDIS_PORT,
+    username=REDIS_USERNAME,
+    password=REDIS_PASSWORD,
 )
