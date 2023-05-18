f149db20702a9a1a2ddee327201394881682d971
PatchingInitiative <126059316+PatchingInitiative@users.noreply.github.com>
2023-05-18 20:47:13 +1000

    - Addressed routing edge case.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -13,6 +13,8 @@ function App() {
             <Route path={"/"} element ={<Home/>} />
             <Route path={"/login"} element ={<Login/>} />
             <Route path={"/create-account"} element ={<CreateAccount/>} />
+            <Route path={"*"} element ={<Home/>} />
+
         </Routes>
 
     );
