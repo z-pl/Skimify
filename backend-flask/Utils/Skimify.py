38484978b90c3af88d8fb1823894b7fcae36cb79
PatchingInitiative <126059316+PatchingInitiative@users.noreply.github.com>
2023-12-13 16:01:06 +1100

    fixed production and improved model
    - Migrated to new OpenAI completion service as prior version is no longer available.
    - GPT 4 Turbo now in use.

--- backend-flask/Utils/Skimify.py
+++ backend-flask/Utils/Skimify.py
@@ -1,4 +1,6 @@
-import openai
+from openai import OpenAI
+
+client = OpenAI(api_key=os.getenv("open_ai_api_key"))
 import os
 from Utils.GPTHelper import split_into_chunks, tokenize
 from dotenv import load_dotenv
@@ -6,7 +8,7 @@ from dotenv import load_dotenv
 
 class SkimifyTool():
     """
-    A tool for converting dot points into more descriptive text using OpenAI GPT-3.5 Turbo.
+    A tool for converting dot points into more descriptive text using OpenAI GPT-4 Turbo.
 
     Attributes:
         None
@@ -17,10 +19,9 @@ class SkimifyTool():
         Initialize the SkimifyTool instance by loading the OpenAPI API key.
         """
         load_dotenv()
-        openai.api_key = os.getenv("open_ai_api_key")
 
     def dotpointToText(self, dotpoint: str) -> str:
-        """Expand the input dot point into a more descriptive text using OpenAI GPT-3.5.
+        """Expand the input dot point into a more descriptive text using OpenAI GPT-4 Turbo.
 
         Args:
             dotpoint (str): A dot point that needs to be expanded into a more descriptive text.
@@ -41,15 +42,13 @@ class SkimifyTool():
 
         dotpoint_conversion = ""
 
-        completion = openai.ChatCompletion.create(
-            model="gpt-4",
-            messages=[
-                {
-                    "role": "user",
-                    "content": f'Expand upon this dotpoint, give information about the topic from your own understanding but do not assume any extra details. But try to keep this relatively brief as well, three-four sentences: "{dotpoint}"'
-                }
-            ], temperature = 0
-        )
+        completion = client.chat.completions.create(model="gpt-4-1106-preview",
+        messages=[
+            {
+                "role": "user",
+                "content": f'Expand upon this dotpoint, give information about the topic from your own understanding but do not assume any extra details. But try to keep this relatively brief as well, three-four sentences: "{dotpoint}"'
+            }
+        ], temperature = 0)
 
         dotpoint_conversion += completion.choices[0].message.content
         return dotpoint_conversion
@@ -65,15 +64,13 @@ class SkimifyTool():
             return "@dp Please reduce input size and try again"
         
 
-        completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        completion = client.chat.completions.create(model="gpt-4-1106-preview",
         messages=[
             {
                 "role": "user",
                 "content": f'Based solely in the information with the text, summarise the key points of this text into dotpoints labelling each dotpoint with only "@dp". The text is "{text}" '
             }
-        ], temperature = 0, max_tokens = 500, n =1
-    )
+        ], temperature = 0, max_tokens = 500, n =1)
 
         data = completion.choices[0].message.content
 
